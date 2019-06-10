eef6e725c4820ae5cdf7e71f51bd210338367118
saini-g <saini.gaurav2205@gmail.com>
2019-06-10 22:40:58 +0530

    added user signup

--- src/cmp/auth.js
+++ src/cmp/auth.js
@@ -1,8 +1,72 @@
 import React, { Component } from 'react';
 
+import './auth.css';
+
 class AuthCmp extends Component {
+
+    constructor(props) {
+        super(props);
+        this.emailElm = React.createRef();
+        this.passwordElm = React.createRef();
+    }
+
+    submitHandler = event => {
+        event.preventDefault();
+        const email = this.emailElm.current.value;
+        const password = this.passwordElm.current.value;
+
+        if (email.trim().length === 0 || password.trim().length === 0) {
+            return;
+        }
+        const reqBody = {
+            query: `
+                mutation {
+                    createUser(userInput: {
+                        email: "${email}",
+                        password: "${password}"
+                    }) {
+                        _id
+                        email
+                    }
+                }
+            `
+        };
+
+        fetch('http://localhost:8000/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(reqBody)
+        })
+        .then(res => {
+
+            if (res.status !== 200 && res.status !== 201) {
+                throw new Error('auth failed!');
+            }
+            return res.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(err => console.log(err));
+    }
+
     render() {
-        return <h1>this is the auth page.</h1>
+        return <form onSubmit={this.submitHandler}>
+            <div className="form-element">
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" ref={this.emailElm} />
+            </div>
+            <div className="form-element">
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" ref={this.passwordElm} />
+            </div>
+            <div className="form-footer">
+                <button type="button">Login</button>
+                <button type="submit">Signup</button>
+            </div>
+        </form>
     }
 }
 
--- src/cmp/nav/MainNav.js
+++ src/cmp/nav/MainNav.js
@@ -6,11 +6,11 @@ import './MainNav.css';
 const MainNav = props => (
     <header className="nav-header">
         <div className="nav-logo">
-            <h3>React Events</h3>
+            <h2>React Events</h2>
         </div>
         <nav className="navbar">
             <ul>
-                <li><NavLink to="/auth">Signup</NavLink></li>
+                <li><NavLink to="/auth">Login</NavLink></li>
                 <li><NavLink to="/events">Events</NavLink></li>
                 <li><NavLink to="/bookings">Bookings</NavLink></li>
             </ul>
